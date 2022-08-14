abf5ef6bf0685b2e4a0d9b43cc9c371e7dc98c5c
cpprian <cpprian456@gmail.com>
2022-08-14 10:28:17 +0200

    init config command

--- cmd/config.go
+++ cmd/config.go
@@ -27,9 +27,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// sopenCmd represents the sopen command
-var sopenCmd = &cobra.Command{
-	Use:   "sopen",
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:   "config",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -38,20 +38,28 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sopen called")
+		fmt.Println("config called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(sopenCmd)
+	rootCmd.AddCommand(configCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// sopenCmd.PersistentFlags().String("foo", "", "A help for foo")
+	configCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// sopenCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+
+	// name
+	// sfont
+	// sfontsize
+	// sopen
+	// spath
+	// spretty
 }
